253f0935fcc4e12e4a0a762c59b29e4e5bd113d8
braChevalier <braden.chevalier@gmail.com>
2017-03-30 18:35:45 -0400

    Extended round times

--- Assets/Scripts/GV.cs
+++ Assets/Scripts/GV.cs
@@ -32,7 +32,7 @@ public class GV {
     public static float Merge_Safe_Zone_Radius = .3f; //size of invisible merge protector
 
     //Main Game
-    public static readonly float[] Game_Length = new float[] { 45, 60, 80, 100, 120 };
+    public static readonly float[] Game_Length = new float[] { 45, 90, 150, 210, 120 };
     public static readonly int[] visibleFormulas = new int[] { 1, 2, 4, 6 };
     public static readonly int Game_Lesson_Max = 4;
 
